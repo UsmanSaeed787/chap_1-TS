03273c7b76c0f7e277f93102503c220bd42fa7d6
UsmanSaeed787 <usmanalisaeed787@gmail.com>
2023-10-03 23:26:18 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+// sayMyName(firstName, lastName)
+function sayMyName(firstName: string, lastName: string) {
+    console.log(`My Full Name is : ${firstName} ${lastName}`);
+}
+sayMyName("Usman", "Saeed");
+
+
+// // Previously: sayMyName(firstName, lastName) { ...
+// function sayMyName(fullName) {
+//     console.log(`You acting kind of shady, ain't callin' me ${fullName}`);
+//     }
+//     sayMyName("Beyoncé", "Knowles");
+//     // ~~~~~~~~~
+//     // Expected 1 argument, but got 2
+
+
+const artist = "Any Person";
+console.log({ artist });
